579285b7db459c16b65b6f3b8ab4516f5a8eb385
Rrhul0 <rr025765@gmail.com>
2023-05-19 21:29:26 +0530

    added quiz.ts filw where the quiz can be added in object form

---
+++ quizes.ts
@@ -0,0 +1,36 @@
+const quizes = {
+    webd: {
+        id: 'webd',
+        name: 'Web Development',
+        desc: 'Test your web development skills with our engaging quiz! Designed for both beginners and experienced developers, this quiz covers HTML, CSS, JavaScript, and popular frameworks. Dive into questions about code syntax, responsive design, best practices, and more. Challenge yourself and compare your knowledge with other developers. Ready to see how you fare? Take our web development quiz now!',
+        questions: [
+            {
+                question: 'Which programming langauge is commonly used in Web development',
+                options: ['C++', 'JavaScript', 'Python', 'Rust'],
+                correctOptionIndex: 1,
+            },
+            {
+                question: 'Which programming langauge is commonly used in Web development',
+                options: ['C++', 'JavaScript', 'Python', 'Rust'],
+                correctOptionIndex: 1,
+            },
+            {
+                question: 'Which programming langauge is commonly used in Web development',
+                options: ['C++', 'JavaScript', 'Python', 'Rust'],
+                correctOptionIndex: 1,
+            },
+            {
+                question: 'Which programming langauge is commonly used in Web development',
+                options: ['C++', 'JavaScript', 'Python', 'Rust'],
+                correctOptionIndex: 1,
+            },
+            {
+                question: 'Which programming langauge is commonly used in Web development',
+                options: ['C++', 'JavaScript', 'Python', 'Rust'],
+                correctOptionIndex: 1,
+            },
+        ],
+    },
+}
+
+export default quizes
