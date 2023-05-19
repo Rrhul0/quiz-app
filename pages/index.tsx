81145fd7137b5a4d17d52caeaeeefe40c49aa069
Rrhul0 <rr025765@gmail.com>
2023-05-19 18:34:30 +0530

    created a non styled home page to start quiz

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,58 @@
+import { useState } from 'react'
+
 export default function Home() {
-    return <main></main>
+    const [startQuiz, setStartQuiz] = useState(false)
+
+    return (
+        <main className='p-6'>
+            <h1 className='py-12 text-center text-9xl font-extrabold'>Quiz</h1>
+            <h2 className='py-4 text-3xl font-bold'>Web development quiz</h2>
+            <div className=''>
+                Test your web development skills with our engaging quiz! Designed for both beginners and experienced
+                developers, this quiz covers HTML, CSS, JavaScript, and popular frameworks. Dive into questions about
+                code syntax, responsive design, best practices, and more. Challenge yourself and compare your knowledge
+                with other developers. Ready to see how you fare? Take our web development quiz now!
+            </div>
+            <section className='py-4'>
+                <h3 className='py-4 text-2xl font-bold'>This Quiz Includes</h3>
+                <ul className='flex flex-col gap-2 text-xl'>
+                    <li>50% Passing Criteria</li>
+                    <li>5 Questions (All compulsory)</li>
+                    <li>10 Minutes</li>
+                    <li>1 Daily Attempt</li>
+                </ul>
+            </section>
+            <div className='flex justify-center'>
+                <button
+                    onClick={() => setStartQuiz(previousValue => !previousValue)}
+                    className='rounded-full bg-slate-600 px-6 py-2 font-semibold text-white'
+                >
+                    Take Quiz
+                </button>
+            </div>
+            {startQuiz && (
+                <div className='absolute inset-0 flex flex-col justify-end bg-slate-300 bg-opacity-40'>
+                    <div className='z-10 flex flex-col gap-4 rounded-t-2xl bg-white p-6'>
+                        <h3 className='py-2 text-2xl font-bold'>Quiz Rules</h3>
+                        <ul className='flex flex-col gap-2 text-xl'>
+                            <li>50% Passing Criteria</li>
+                            <li>5 Questions (All compulsory)</li>
+                            <li>10 Minutes</li>
+                        </ul>
+                        <div className='rounded-lg bg-slate-200 px-3 py-1'>
+                            This quiz can only be attempted once daily.
+                        </div>
+                        <div className='flex justify-center'>
+                            <button
+                                onClick={() => {}}
+                                className='rounded-full bg-slate-600 px-6 py-2 font-semibold text-white'
+                            >
+                                Start
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </main>
+    )
 }
