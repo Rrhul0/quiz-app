b88c25600b4e99adfbef6b600624adae3ce25890
Rrhul0 <rr025765@gmail.com>
2023-05-21 00:23:43 +0530

    added more msgs for good score and great score

--- pages/result.tsx
+++ pages/result.tsx
@@ -2,6 +2,7 @@ import SomethingWrong from '@/components/somethingWrong'
 import { QuizContext } from '@/contexts/quizContext'
 import formatTime from '@/lib/formatTime'
 import quizes from '@/quizes'
+import Link from 'next/link'
 import { useContext } from 'react'
 
 const quizIds = Object.keys(quizes)
@@ -32,15 +33,26 @@ export default function ResultPage() {
             <div className='mx-2 flex flex-col items-center gap-3 rounded-2xl border bg-white p-4 text-center drop-shadow-lg'>
                 <div className='text-lg text-green-600'>You Scored:</div>
                 <div className='text-3xl font-semibold text-purple-500'>{score}%</div>
-                <p className='text-lg font-bold'>Oh Snap, You can do better!</p>
+                <p className='text-lg font-bold'>
+                    {score <= 40
+                        ? 'Oh Snap, You can do better!'
+                        : score <= 60
+                        ? 'Good, But you can do better!'
+                        : 'Great, You did fantastic!'}
+                </p>
                 <p className='px-4 '>
-                    Success is not final, failure is not fatal; It is the courage to continue that counts.
+                    {score <= 40
+                        ? 'Success is not final, failure is not fatal; It is the courage to continue that counts.'
+                        : score <= 60
+                        ? 'Embrace each challenge as an opportunity to learn, grow, and do better than before.'
+                        : "You've done great, and I encourage you to keep up the excellent work!"}
                 </p>
             </div>
             <div className='flex justify-between p-8'>
                 <div>
                     <div className='font-semibold'>Your Score</div>
                     <div className='text-3xl font-bold tracking-wide text-purple-500'>{correct}/5</div>
+                    <div className='pt-2 text-sm'>Avg. Score: 2</div>
                 </div>
                 <div>
                     <div className='font-semibold'>Time Taken</div>
@@ -48,7 +60,13 @@ export default function ResultPage() {
                         {passedMinutes}:{passedSeconds}
                         <span className='text-base font-semibold tracking-normal text-black'> mins</span>
                     </div>
+                    <div className='pt-2 text-sm '>Avg. Time: 01:34 mins</div>
                 </div>
+            </div>
+            <div className='flex w-full justify-center pt-8'>
+                <Link href='/' className='rounded-full bg-slate-600 px-6 py-2 font-semibold text-white'>
+                    Return to Home
+                </Link>
             </div>
         </div>
     )
